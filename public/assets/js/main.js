2ffd568f55a2c6d9cb3aaccc32485b2f3baa6eb4
blumrj <marijaf202@gmail.com>
2023-09-15 20:01:14 +0200

    adding delete functionality

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,7 @@
 var listsContainer = document.querySelector(".lists")
 var btnCreateList = document.querySelector("#btnCreateList")
 var tbCreateList = document.querySelector("#tbCreateList")
+var modalsDiv = document.querySelector("#modals")
 
 
 
@@ -10,23 +11,8 @@ const showAllLists = async () => {
         if(lists.length){
             var html = ''
             lists.forEach(list => {
-                html += `
-                <div class='col'>
-                    <div class="card h-100">
-                        <div class="card-header row justify-content-between mx-0">
-                            ${list.name} 
-                            <span class='col-auto'>
-                            <i class="bi bi-plus-circle addBtn"></i>
-                            <i class="bi bi-pen editBtn"></i>
-                            <i class="bi bi-trash3 deleteBtn" data-bs-toggle="modal" data-bs-target="#backdropModal"></i>
-                            </span>
-                        </div>
-                        <div class="card-body">
-                        <h6 class="card-subtitle">This list is empty</h6>
-
-                        </div>
-                    </div>
-                </div>`
+                html += renderList(list._id, list.name)
+                renderModal(list._id, list.name)
             });
             listsContainer.innerHTML = html
         }
@@ -35,6 +21,26 @@ const showAllLists = async () => {
     }
 }
 showAllLists()
+
+function renderList(id, name){
+    var html = `<div class='col'>
+    <div class="card h-100">
+        <div class="card-header row justify-content-between mx-0">
+            ${name} 
+            <span class='col-auto'>
+            <i class="bi bi-plus-circle addBtn"></i>
+            <i class="bi bi-pen editBtn"></i>
+            <i class="bi bi-trash3 deleteBtn" data-bs-toggle="modal" data-bs-target="#modal-${id}"></i>
+            </span>
+        </div>
+        <div class="card-body">
+        <h6 class="card-subtitle">This list is empty</h6>
+        </div>
+    </div>
+</div>`
+
+return html
+}
 
 const createList = async () => {
     try {
@@ -47,31 +53,43 @@ const createList = async () => {
     }
 }
 
+const deleteList = async (id) => {
+    // try {
+    //     console.log(id);
+    //     var listID = id
+    //     await axios.delete(`/api/v1/lists/${listID}`)
+    //     showAllLists()
+    // } catch (error) {
+    //     console.log(error);
+    // }
+    console.log("alo");
+}
+
 btnCreateList.addEventListener("click", () => {
     createList()
 })
 
-function renderModal(){
-    var html = `<div class="modal fade" id="backdropModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+function renderModal(id, name){
+    var html = `<div class="modal fade" id="modal-${id}" tabindex="-1" aria-labelledby="modal-${id}-label" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5" id="modal-${id}-label">Confirm your action</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-            Are you sure you want to delete this list?
+            Are you sure you want to delete ${name}?
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Yes</button>
+        <button type="button" class="btn btn-primary" onclick="deleteList(${id})">Yes</button>
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
         </div>
       </div>
     </div>
   </div>`
+
+  modalsDiv.innerHTML += html
 }
 
-var deleteBtns = document.querySelector(".card")
-console.log(deleteBtns);
 
 
